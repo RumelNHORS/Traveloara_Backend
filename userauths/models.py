ca06c0d024a0d8d14a2ce82c9b0a041aea998d73
RumelNHORS <rumelnhors52@gmail.com>
2024-09-24 02:30:34 -0400

    create User Profile Model.

--- userauths/models.py
+++ userauths/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db.models.signals import post_save
+from django.conf import settings
 
 
 # Choices for gender and identity types
@@ -77,5 +78,46 @@ class User(AbstractUser):
             self.is_admin = False
         
         super().save(*args, **kwargs)
+
+
+# Profile model linked to User via OneToOneField
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.FileField(upload_to='profile_pictures/', default="profile_pictures/default.jpg", null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    # Identity verification
+    identity_type = models.CharField(max_length=100, choices=IDENTITY_TYPE, null=True, blank=True)
+    identity_image = models.FileField(upload_to='identity_pictures/', default="identity_pictures/default.jpg", null=True, blank=True)
     
+    verified = models.BooleanField(default=False)
+
+    # Use DateField to store only the date, not the time
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    class Meta:
+        # Order profiles by descending dates
+        ordering = ['-created_at']
+    
+    def __str__(self):
+        if self.full_name:
+            return f"{self.user.first_name}" 
+        else:
+            return f"{self.user.username}"
+
+
+# Signal handlers to create and save Profile objects upon User creation
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+# Connect the signal handlers to the User model
+post_save.connect(create_user_profile, sender=User)
+post_save.connect(save_user_profile, sender=User)
+
 
