13f4db5290c1886991bed3eec2689207305cc443
RumelNHORS <rumelnhors52@gmail.com>
2024-09-26 03:04:37 -0400

    update the room models.

--- hotel/serializers.py
+++ hotel/serializers.py
@@ -10,6 +10,14 @@ class PropertySerializer(serializers.ModelSerializer):
         model = hotel_models.Property
         fields = '__all__'
 
+    def update(self, instance, validated_data):
+        # If 'image' is not in validated_data, we retain the existing image
+        image = validated_data.get('image', None)
+        if image is None:
+            # If no image provided, keep the current image
+            validated_data['image'] = instance.image
+        return super().update(instance, validated_data)
+
 
 # Room Serializer
 class RoomSerializer(serializers.ModelSerializer):
